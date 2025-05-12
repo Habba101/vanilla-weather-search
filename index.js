a172da302568d01d417767b327ebbb803018d43a
Habba101 <rahabamotaung@yahoo.com>
2025-05-12 11:28:58 +0200

    updated css

---
+++ index.js
@@ -0,0 +1,15 @@
+function searchCity(city) {
+  let apiKey = "d0e50fa0aabd593o0b84ed0t8acf23ad";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+
+  searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
